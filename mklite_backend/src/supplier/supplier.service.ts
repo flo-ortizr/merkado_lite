047c18f058ba2c5ec559bb438858ea296b24fae7
Flora <flora.ortiz@ucb.edu.bo>
2025-11-19 21:24:24 -0400

    feat: supplier service methods

--- mklite_backend/src/supplier/supplier.service.ts
+++ mklite_backend/src/supplier/supplier.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from "@nestjs/common";
 import { AppDataSource } from "src/data-source";
 import { Supplier } from "src/supplier/supplier.entity";
+import { CreateSupplierDto } from "./dto/create_supplier.dto";
+import { UpdateSupplierDto } from "./dto/update_supplier.dto";
 
 @Injectable()
 export class SupplierService {
-    async createSupplier(supplier: Supplier) {
+    async createSupplier(dto: CreateSupplierDto){
+        const supplier = AppDataSource.manager.create(Supplier, dto);
         return await AppDataSource.manager.save(Supplier, supplier);
     }
 
@@ -20,7 +23,8 @@ export class SupplierService {
         return await AppDataSource.manager.delete(Supplier, {id_supplier: id});
     }
 
-    async updateSupplier(id: number, supplier: Supplier) {
-        return await AppDataSource.manager.update(Supplier, {id_supplier: id}, supplier);
+    async updateSupplier(id: number, dto: UpdateSupplierDto) {
+        await AppDataSource.manager.update(Supplier, { id_supplier: id }, dto);
+        return this.getSupplierById(id);
     }
 }
