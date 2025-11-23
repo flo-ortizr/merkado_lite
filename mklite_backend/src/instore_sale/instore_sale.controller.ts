276720b99bb745e285b76e6fc4640b87956f23fe
Flora <flora.ortiz@ucb.edu.bo>
2025-11-23 18:46:40 -0400

    feat: inStore sale controller

--- mklite_backend/src/instore_sale/instore_sale.controller.ts
+++ mklite_backend/src/instore_sale/instore_sale.controller.ts
@@ -1,34 +1,18 @@
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { InStoreSaleService } from './instore_sale.service';
+import { CreateInStoreSaleDto } from './dto/create_instore_sale.dto';
 
-import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
-import { InStoreSale } from './instore_sale.entity';
-import { InstoreSaleService } from './instore_sale.service';
+@Controller('/instore-sale')
+export class InStoreSaleController {
+  constructor(private readonly saleService: InStoreSaleService) {}
 
-@Controller('instore-sale')
-export class InstoreSaleController {
-    constructor(private readonly instoreSaleService: InstoreSaleService) {}
+  @Post()
+  async createSale(@Body() dto: CreateInStoreSaleDto) {
+    return this.saleService.createSale(dto);
+  }
 
-    @Post()
-    async createInstoreSale(@Body() instoresale: InStoreSale) {
-        return await this.instoreSaleService.createInstoreSale(instoresale);
-    }
-
-    @Get()
-    async getAllInstoreSales() {
-        return await this.instoreSaleService.getAllInstoreSales();
-    }
-
-    @Get(':id')
-    async getInstoreSaleById(@Param('id') id: number) {
-        return await this.instoreSaleService.getInstoreSaleById(id);
-    }
-
-    @Delete(':id')
-    async deleteInstoreSale(@Param('id') id: number) {
-        return await this.instoreSaleService.deleteInstoreSale(id);
-    }
-
-    @Put(':id')
-    async updateInstoreSale(@Param('id') id: number, @Body() instoresale: InStoreSale) {
-        return await this.instoreSaleService.updateInstoreSale(id, instoresale);
-    }
+  @Get('/user/:userId')
+  async getSalesByUser(@Param('userId') userId: number) {
+    return this.saleService.getSalesByUser(userId);
+  }
 }
