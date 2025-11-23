635a7d4ccb68da251e15fb74ee29a464d5dc01bc
Flora <flora.ortiz@ucb.edu.bo>
2025-11-23 18:12:36 -0400

    feat: historial pedidos

--- mklite_backend/src/order/dto/order_history.dto.ts
+++ mklite_backend/src/order/dto/order_history.dto.ts
@@ -3,11 +3,7 @@ export class OrderHistoryDto {
   order_date: Date;
   status: string;
   total: number;
-  payment_method: string;
-  delivery_method?: string;
   details: {
     product_name: string;
-    quantity: number;
-    subtotal: number;
   }[];
 }
