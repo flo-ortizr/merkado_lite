ca75500a00dec27e7db77e76e63f86ceb98ee47c
Flora <flora.ortiz@ucb.edu.bo>
2025-12-18 07:22:11 -0400

    page.tsx provisional

---
+++ mklite_frontend/app/perfil/page.tsx
@@ -0,0 +1,8 @@
+export default function PerfilPage() {
+  return (
+    <div style={{ padding: "2rem", color: "white" }}>
+      <h1>Pantalla de Perfil</h1>
+      <p>Vista en construcción</p>
+    </div>
+  );
+}
